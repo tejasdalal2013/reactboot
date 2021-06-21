263220e01d204e2f94fdf7ec99359f5fc559c995
Tejas <tejasdalal2013@gmail.com>
2021-06-21 22:15:41 +0530

    second commit

---
+++ src/Student.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Student extends Component{
+    render(){
+        return <h1>Hello Folks Done With My First Task</h1>;
+    }
+}
+
+export default Student;
