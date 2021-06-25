dd77e800cb740f2a29272f4d82039a70e1fbe91f
Tejas <tejasdalal2013@gmail.com>
2021-06-25 09:46:23 +0530

    Forth Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Today With Me
-        </a>
-      </header>
-    </div>
+const App =() =>{
+  const [num,setNum] = useState(0);
+  const [nums,setNums] = useState(0);
+
+  useEffect(()=>{
+    alert("I Am Clicked") 
+  },[num])
+    
+  return(
+    <>
+    <button 
+    onClick={() =>
+      {setNum(num+1);
+    }}
+  >
+    Click Me{num}
+  </button>
+  <button 
+    onClick={() =>
+      {setNums(nums+1);
+    }}
+  >
+    Click Me{nums}
+  </button>
+  </>
+
+  
+  
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Student from './Student';
+import App from './App';
 
-ReactDOM.render(<Student />,document.getElementById('root'))
+ReactDOM.render(<App />,
+    document.getElementById('root'))
